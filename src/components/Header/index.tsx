45756a488bc66c1361c20f62f3ef9e225ea38af1
Wander Aquino <wander.aquino@gmail.com>
2021-08-31 14:48:02 -0300

    Tracking Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { Flex } from "@chakra-ui/react";
+import { Logo } from "../Logo";
+export function Header () {
+    return (
+        <Flex 
+            as="header" 
+            w="100%" 
+            maxW="760px" 
+            alignItems="center" 
+            justifyContent="center"
+            marginX="auto"
+            height="60px"
+            >
+            <Logo />
+        </Flex>
+    )
+}
