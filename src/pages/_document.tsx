a75dbcc7f5a87d24282a6a440d12f74c286a2c1f
Wander Aquino <wander.aquino@gmail.com>
2021-08-30 23:55:31 -0300

    First Commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+    render () {
+        return (
+            <Html>
+                <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&display=swap" rel="stylesheet" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
