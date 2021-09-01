bb6ae445e006476b85527e41ac8ed82a918d0201
Wander Aquino <wander.aquino@gmail.com>
2021-08-31 22:22:57 -0300

    Rendering HomeBanner component on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import { Header } from "../components/Header";
+import { HomeBanner } from "../components/HomeBanner";
 
 export default function Home() {
   return (
+    <>
     <Header />
+    <HomeBanner />
+    </>
   )
 }
