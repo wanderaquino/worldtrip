4554d24f345e1d4accdb618608b8e916a752837e
Wander Aquino <wander.aquino@gmail.com>
2021-09-01 23:24:28 -0300

    Rendenring TravelTypes component on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
+import { Box } from "@chakra-ui/react";
 import { Header } from "../components/Header";
 import { HomeBanner } from "../components/HomeBanner";
+import { TravelTypesSection } from "../components/TravelTypesSection";
 
 export default function Home() {
   return (
     <>
     <Header />
     <HomeBanner />
+    <Box mt="2rem">
+      <TravelTypesSection/>
+    </Box>
     </>
   )
 }
