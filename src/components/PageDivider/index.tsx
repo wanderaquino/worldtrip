b1b9a79f98385228965598e70e5a8a493b3be40d
Wander Aquino <wander.aquino@gmail.com>
2021-09-02 00:38:47 -0300

    Tracking PageDivider component

--- src/components/PageDivider/index.tsx
+++ src/components/PageDivider/index.tsx
@@ -3,8 +3,11 @@ import { Box, Flex } from "@chakra-ui/react";
 
 export function PageDivider() {
     return (
-        <Flex w="100%" px={["2rem", "7rem"]} alignItems="center">
-            <Box border="solid" height="1px" color="black" w="5rem" />
+        <Flex w="100%" px={["2rem", "7rem"]} alignItems="center" justifyContent="center">
+            <Box 
+                width="5rem"
+                height="1.2px"
+                bg="gray.800"/>
         </Flex>
     )   
 }
