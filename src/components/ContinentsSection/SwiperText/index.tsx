132836ae7dd5ccd354bddaf623950783b1ca4e1f
Wander Aquino <wander.aquino@gmail.com>
2021-09-05 17:45:31 -0300

    Swiper Text component

---
+++ src/components/ContinentsSection/SwiperText/index.tsx
@@ -0,0 +1,28 @@
+import {Heading, Text, Flex, Link as ChakraLink} from "@chakra-ui/react"
+import  Link  from "next/link" ;
+
+interface SwiperTextProps {
+    headerText: string,
+    subText: string
+}
+
+export function SwiperText ({headerText, subText} : SwiperTextProps) {
+    return (
+        <Flex
+            position="absolute"
+            align="center"
+            justify="center"
+            color="gray.200">
+                <Link href="">
+                    <ChakraLink _hover={{
+                         color: "#FFBA08"
+                    }}>
+                        <Flex flexDir="column" align="center">
+                            <Heading fontSize={["1.5rem","2rem"]}>{headerText}</Heading>
+                            <Text fontSize={["1rem","1rem"]}>{subText}</Text>
+                        </Flex>
+                    </ChakraLink>
+                </Link>
+        </Flex>
+    )
+}
