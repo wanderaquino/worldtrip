6295845fd72ffe5c350a2c7735ac550d17202adb
Wander Aquino <wander.aquino@gmail.com>
2021-09-04 23:34:49 -0300

    Adding continent swiper component

--- src/components/ContinentsSection/index.tsx
+++ src/components/ContinentsSection/index.tsx
@@ -1,20 +1,21 @@
 import { Flex, Heading } from "@chakra-ui/react";
-
+import ContinentSwiper from "./ContinentsSwiper";
 
 export function ContinentsSection () {
     return (
+        <>
         <Flex
             as="section" 
             w="100%"
-            h="5rem"
+            h="3rem"
             mt="2rem"
             px={["1rem","7rem"]}
             justify="center"
         >       
             <Flex   
                 align="center"
-                flexDirection="column"
-            >
+                flexDirection="column">
+
                 <Heading 
                     as="h1" 
                     fontWeight="500"
@@ -24,5 +25,7 @@ export function ContinentsSection () {
                 </Heading>
             </Flex>
         </Flex>
+        <ContinentSwiper />
+    </>
     )
 }
