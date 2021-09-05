775a8f15c63b6025d318d8d2a616380c1890770e
Wander Aquino <wander.aquino@gmail.com>
2021-09-04 23:34:06 -0300

    Adding custom swiper style for continents swyper component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import {AppProps} from "next/app";
-import {theme} from "../styles/theme"
-
+import {theme} from "../styles/theme";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import "../styles/swiperCustom.css";
 
 function MyApp({ Component, pageProps } : AppProps) {
   return (
