0b871db374f5ff9757eb083e915c46b786d7e23d
Wander Aquino <wander.aquino@gmail.com>
2021-09-06 22:25:16 -0300

    Repassing href for continent swipe item

--- src/components/ContinentsSection/SwiperText/index.tsx
+++ src/components/ContinentsSection/SwiperText/index.tsx
@@ -2,21 +2,20 @@ import {Heading, Text, Flex, Link as ChakraLink} from "@chakra-ui/react"
 import  Link  from "next/link" ;
 
 interface SwiperTextProps {
+    href: string,
     headerText: string,
     subText: string
 }
 
-export function SwiperText ({headerText, subText} : SwiperTextProps) {
+export function SwiperText ({href, headerText, subText} : SwiperTextProps) {
     return (
         <Flex
             position="absolute"
             align="center"
             justify="center"
             color="gray.200">
-                <Link href="">
-                    <ChakraLink _hover={{
-                         color: "#FFBA08"
-                    }}>
+                <Link href={href}>
+                    <ChakraLink _hover={{color: "#FFBA08"}}>
                         <Flex flexDir="column" align="center">
                             <Heading fontSize={["1.5rem","2rem"]}>{headerText}</Heading>
                             <Text fontSize={["1rem","1rem"]}>{subText}</Text>
