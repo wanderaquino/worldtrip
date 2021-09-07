109cf6e5a202a90d021e4ec01fa822d1eb2b6ba5
Wander Aquino <wander.aquino@gmail.com>
2021-09-06 22:24:43 -0300

    Tracking individual Continent page

---
+++ src/pages/continent/[continent].tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+import { Header } from "../../components/Header"
+import { HomeBanner } from "../../components/HomeBanner"
+
+export default function Continent () {
+    return (
+    <>
+        <Head>
+            <title>WorldTrip | Home</title>
+        </Head>
+        <Header />
+    </>
+    )
+}
