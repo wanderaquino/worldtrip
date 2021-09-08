c53d76f21db4f71fcd6fdb5284eb314a58d4da57
Wander Aquino <wander.aquino@gmail.com>
2021-09-07 21:34:38 -0300

    Adding continentPage flag for previous page button

--- src/pages/continent/[continent].tsx
+++ src/pages/continent/[continent].tsx
@@ -1,6 +1,5 @@
 import Head from "next/head"
 import { Header } from "../../components/Header"
-import { HomeBanner } from "../../components/HomeBanner"
 
 export default function Continent () {
     return (
@@ -8,7 +7,7 @@ export default function Continent () {
         <Head>
             <title>WorldTrip | Home</title>
         </Head>
-        <Header />
+        <Header isContinentPage={true}/>
     </>
     )
 }
