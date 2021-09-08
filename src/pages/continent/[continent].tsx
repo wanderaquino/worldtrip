9ac120c02155c1dffb645f6ef06215de2c57f3e4
Wander Aquino <wander.aquino@gmail.com>
2021-09-07 22:33:47 -0300

    Adding continent banner img

--- src/pages/continent/[continent].tsx
+++ src/pages/continent/[continent].tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import {Flex, Img} from "@chakra-ui/react";
 import { Header } from "../../components/Header"
 
 export default function Continent () {
@@ -8,6 +9,14 @@ export default function Continent () {
             <title>WorldTrip | Home</title>
         </Head>
         <Header isContinentPage={true}/>
+        <Flex
+            w="100%" 
+            maxW="1260px"
+            h="20rem"
+            mx="auto">
+            <Img 
+                href="/images/Europe.jpg" />
+        </Flex>
     </>
     )
 }
