8ef19ebe6df7e523e20698ff54d7133b7846b1e6
Wander Aquino <wander.aquino@gmail.com>
2021-09-07 21:34:15 -0300

    Adding previous page button for continent page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,47 @@
-import { Flex } from "@chakra-ui/react";
-import { Logo } from "../Logo";
-export function Header () {
+import { Flex, Box } from "@chakra-ui/react";
+import { Logo } from "./Logo";
+import { PreviousButton } from "./PreviousButton";
+
+interface HeaderProps {
+    isContinentPage?: boolean 
+}
+
+export function Header ({isContinentPage = false} : HeaderProps) {
+
     return (
+        
+        isContinentPage ? (            
+        
         <Flex 
             as="header" 
             w="100%" 
-            maxW="760px" 
-            alignItems="center" 
-            justifyContent="center"
+            maxW="1260px" 
+            align="center" 
             marginX="auto"
+            justify="center"
+            justifyContent="center"
             height="4rem"
+            position="relative"
             >
-            <Logo />
+                <Box position="absolute" left={0}>
+                    <PreviousButton />
+                </Box>
+                <Box>
+                    <Logo />
+                </Box>
+        </Flex>) 
+        
+        :
+
+        <Flex 
+                as="header" 
+                w="100%" 
+                maxW="760px" 
+                alignItems="center" 
+                justifyContent="center"
+                marginX="auto"
+                height="4rem">
+                <Logo />
         </Flex>
     )
 }
