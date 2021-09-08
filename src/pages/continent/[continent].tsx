c25f5efe769b1edd17f1ed3b56b46fe9e95b9176
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-09-08 00:46:34 -0300

    Adding banner for continent page

--- src/pages/continent/[continent].tsx
+++ src/pages/continent/[continent].tsx
@@ -11,14 +11,26 @@ export default function Continent () {
         <Header isContinentPage={true}/>
         <Flex
             w="100%" 
-            maxW="1260px"
             h="20rem"
-            mx="auto">
-            <Img 
-                w="inherit"
-                h="inherit"
-                src="/images/Europe.jpg" 
-                objectFit="cover"/>
+            mx="auto"
+            position="relative"
+            >
+            <Box w="inherit" h="inherit" bg="rgb(0,0,0, 0.6)" position="absolute"/>
+            <Img w="inherit" maxW="inherit" h="inherit" src="/images/Europe.jpg" objectFit="cover"/>
+
+            <Box
+                position="absolute" 
+                px={["1rem", "3rem"]}
+                bottom={10}
+                >
+                <Heading
+                    color="white"
+                    fontWeight="700"
+                    fontSize="2rem"
+                    >Europa
+                </Heading>
+            </Box>
+
         </Flex>
     </>
     )
