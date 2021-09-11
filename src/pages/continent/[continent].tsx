471c2722767a2bb40d8236737f55ff58bbfd3fee
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-09-11 02:08:56 -0300

    Adding city card

--- src/pages/continent/[continent].tsx
+++ src/pages/continent/[continent].tsx
@@ -35,7 +35,7 @@ export default function Continent () {
         <Flex 
             as="section"
             w="100%" 
-            h="15rem"
+            h="14rem"
             mx="auto"
             my={["1.5rem","2rem"]}
             >
@@ -44,13 +44,13 @@ export default function Continent () {
                 px={["1rem", "3rem"]}>
                 
                 <Grid templateColumns="repeat(2, 1fr)" gridColumnGap={5}>
-                    <GridItem ScolSpan={1} my="auto">
+                    <GridItem colSpan={1} my="auto">
                         <Text textAlign="justify">
                         A Europa é, por convenção, um dos seis continentes do mundo. Compreendendo a península ocidental da Eurásia, a Europa geralmente divide-se da Ásia a leste pela divisória de águas dos montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste.
                         </Text>
                     </GridItem>
 
-                    <GridItem ScolSpan={1} m="auto auto">
+                    <GridItem colSpan={1} m="auto auto">
                         <Flex justifyContent="space-between" w="20rem">
                             <Flex flexDirection="column">
                                 <Text textAlign="center" fontSize="2rem" color="#FFBA08" fontWeight="600" >50</Text>
@@ -68,9 +68,43 @@ export default function Continent () {
                             </Flex>
                         </Flex>
                     </GridItem>
-
                 </Grid>
             </Flex>
+        </Flex>
+        <Flex as="section" px={["1rem", "3rem"]} flexDir="column" my="1.5rem">
+            <Heading fontSize="2rem" fontWeight="600">Cidades +100</Heading>
+            <Grid my="1.5rem" templateColumns="repeat(4, 1fr)" gap={5} >
+                <GridItem>
+                    <Flex 
+                        flexDir="column"  
+                        overflow="hidden"
+                        borderRadius=".2rem" 
+                        w="240px">
+
+                        <Img src="/images/UnitedKing.png"/>
+                        <Flex 
+                            flexDir="row" 
+                            justifyContent="space-between" 
+                            alignItems="center" 
+                            padding="1rem" 
+                            border="solid" 
+                            borderWidth=".1rem" 
+                            borderColor="rgba(255, 186, 8, 0.5)" 
+                            borderTop="none">
+
+                            <Flex flexDir="column">
+                                <Heading as="h2" fontSize="1.25rem">Londres</Heading>
+                                <Text fontWeight="normal" mt=".5rem" fontSize="1rem" color="#999999">Reino Unido</Text>
+                            </Flex>
+                            <Flex my="1.5rem">
+                                <Box borderRadius="100%" w="2.5rem" h="2.5rem">
+                                    <Img src="/images/EnglandFlag.png" w="2.5rem" h="2.5rem" objectFit="fill" />
+                                </Box>
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                </GridItem>
+            </Grid>
         </Flex>
     </>
     )
