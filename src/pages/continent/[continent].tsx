038981cef05a57f2d6fb3e5e945b1fa0f1ea7ac0
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-09-11 02:33:51 -0300

    Responsive Banner

--- src/pages/continent/[continent].tsx
+++ src/pages/continent/[continent].tsx
@@ -1,37 +1,69 @@
 import Head from "next/head"
-import {Flex, Img, Box, Heading, Text, Grid, GridItem}from "@chakra-ui/react";
+import {Flex, Img, Box, Heading, Text, Grid, GridItem, useBreakpointValue}from "@chakra-ui/react";
 import { Header } from "../../components/Header"
 
 export default function Continent () {
+
+    const isWideScreen = useBreakpointValue({
+        md: true
+    });
+    
     return (
     <>
         <Head>
             <title>WorldTrip | Home</title>
         </Head>
         <Header isContinentPage={true}/>
-        <Flex
-            w="100%" 
-            h="20rem"
-            mx="auto"
-            position="relative"
-            >
-            <Box w="inherit" h="inherit" bg="rgb(0,0,0, 0.6)" position="absolute"/>
-            <Img w="inherit" maxW="inherit" h="inherit" src="/images/Europe.jpg" objectFit="cover"/>
 
-            <Box
-                position="absolute" 
-                px={["1rem", "3rem"]}
-                bottom={10}
+        {
+            !isWideScreen ? (
+            <Flex
+                w="100%" 
+                h="20rem"
+                mx="auto"
+                position="relative"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
                 >
-                <Heading
-                    color="white"
-                    fontWeight="700"
-                    fontSize="2rem"
-                    >Europa
-                </Heading>
-            </Box>
-
-        </Flex>
+                <Box w="inherit" h="inherit" bg="rgb(0,0,0, 0.6)" position="absolute"/>
+                <Img w="inherit" maxW="inherit" h="inherit" src="/images/Europe.jpg" objectFit="cover"/>
+                <Box
+                    position="absolute" 
+                    px={["1rem", "3rem"]}
+                    >
+                    <Heading
+                        color="white"
+                        fontWeight="700"
+                        fontSize="2rem"
+                        >Europa
+                    </Heading>
+                </Box>
+            </Flex>
+            ) : (
+            <Flex
+                w="100%" 
+                h="20rem"
+                mx="auto"
+                position="relative"
+                >
+                <Box w="inherit" h="inherit" bg="rgb(0,0,0, 0.6)" position="absolute"/>
+                <Img w="inherit" maxW="inherit" h="inherit" src="/images/Europe.jpg" objectFit="cover"/>
+                <Box
+                    position="absolute" 
+                    px={["1rem", "3rem"]}
+                    bottom={10}
+                    >
+                    <Heading
+                        color="white"
+                        fontWeight="700"
+                        fontSize="2rem"
+                        >Europa
+                    </Heading>
+                </Box>
+            </Flex>
+            )
+        }
         <Flex 
             as="section"
             w="100%" 
