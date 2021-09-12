30d1d247b16f42575f1d984f2f53bd90b9ea542b
Wander Aquino <wander.aquino@gmail.com>
2021-09-12 17:15:32 -0300

    Decrease font size for continent short description

--- src/pages/continent/[continent].tsx
+++ src/pages/continent/[continent].tsx
@@ -59,7 +59,7 @@ export default function Continent () {
                         
                         <Grid templateColumns="1">
                             <GridItem my="auto">
-                                <Text textAlign="justify">
+                                <Text textAlign="justify" fontSize=".875rem">
                                 A Europa é, por convenção, um dos seis continentes do mundo. Compreendendo a península ocidental da Eurásia, a Europa geralmente divide-se da Ásia a leste pela divisória de águas dos montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste.
                                 </Text>
                             </GridItem>
