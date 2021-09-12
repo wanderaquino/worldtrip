ebff45f3e471fba095fd3839938a733e2dfe35fd
Wander Aquino <wander.aquino@gmail.com>
2021-09-12 20:13:09 -0300

    Remove unused console.log

--- src/pages/api/continents/index.ts
+++ src/pages/api/continents/index.ts
@@ -2,7 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next"
 import continents from "../../../../db.json"
 
 export default function Continents (request:NextApiRequest, response:NextApiResponse) {
-    console.log(request.query)
     if(request.method === "GET") {
         response.status(200).json(continents)
     } else {
